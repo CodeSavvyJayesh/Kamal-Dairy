63bb23588840a563ecce86def0ef3d35a07fb1ba
Jayesh <jayeshdhamal327@gmail.com>
2026-02-10 05:58:25 +0530

    Make Auth UI premium and fix CSS

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "../components/Auth.css";
+import "./Auth.css";
 
 function Auth() {
   const [isLogin, setIsLogin] = useState(true);
